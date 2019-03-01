02c14836c182fb764da308ddce3caac03d92d7da
johanpoccard <47979081+johanpoccard@users.noreply.github.com>
2019-03-01 18:02:54 +0100

    Création dossier

---
+++ src/modules/PRAO/PRAOAttitudeControl.h
@@ -0,0 +1,14 @@
+//
+// Created by Johan on 01.03.2019.
+//
+
+#ifndef FIRMWARE_PRAOATTITUDECONTROL_H
+#define FIRMWARE_PRAOATTITUDECONTROL_H
+
+
+class PRAOAttitudeControl {
+
+};
+
+
+#endif //FIRMWARE_PRAOATTITUDECONTROL_H
