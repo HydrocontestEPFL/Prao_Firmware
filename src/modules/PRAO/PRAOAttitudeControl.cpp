c22ab6ce8631f7da7cf38efe7a25698144bd9683
johanpoccard <47979081+johanpoccard@users.noreply.github.com>
2019-03-05 18:36:26 +0100

    J'avance

--- src/modules/PRAO/PRAOAttitudeControl.cpp
+++ src/modules/PRAO/PRAOAttitudeControl.cpp
@@ -35,4 +35,71 @@
  *
  ****************************************************************************/
 
+#include <px4_config.h>
+#include <px4_tasks.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <math.h>
+#include <poll.h>
+#include <time.h>
+#include <drivers/drv_hrt.h>
+#include <uORB/uORB.h>
+#include <uORB/topics/vehicle_global_position.h>
+#include <uORB/topics/position_setpoint_triplet.h>
+#include <uORB/topics/vehicle_attitude.h>
+#include <uORB/topics/vehicle_status.h>
+#include <uORB/topics/vehicle_attitude_setpoint.h>
+#include <uORB/topics/manual_control_setpoint.h>
+#include <uORB/topics/actuator_controls.h>
+#include <uORB/topics/vehicle_global_position.h>
+#include <uORB/topics/parameter_update.h>
+#include <parameters/param.h>
+#include <lib/ecl/geo/geo.h>
+#include <perf/perf_counter.h>
+#include <systemlib/err.h>
+#include <matrix/math.hpp>
+
 #include "PRAOAttitudeControl.h"
+
+extern "C" __EXPORT int fw_att_control_main(int argc, char *argv[]);
+{
+    PX4_INFO("Hello water!");
+
+}
+
+//Mettre tous les paramètres à utiliser
+
+//Faire toutes les subscriptions ( peut etre besoin de mettre un int devant )
+// Maybe limiter l'update rate avec orb_set_interval (voir dans exemples/uuv_exemple)
+
+int _att_sp_sub = orb_subscribe(ORB_ID(vehicle_attitude_setpoint));
+int _ctrl_state_sub = orb_subscribe(ORB_ID(control_state));
+int _accel_sub = orb_subscribe_multi(ORB_ID(sensor_accel), 0);
+int _vcontrol_mode_sub = orb_subscribe(ORB_ID(vehicle_control_mode));
+int _distance_sensor_sub = orb_subscribe(ORB_ID(distance_sensor));
+int _params_sub = orb_subscribe(ORB_ID(parameter_update));
+int _manual_sub = orb_subscribe(ORB_ID(manual_control_setpoint));
+int _global_pos_sub = orb_subscribe(ORB_ID(vehicle_global_position));
+int _local_pos_sub = orb_subscribe(ORB_ID(vehicle_local_position));
+int _vehicle_status_sub = orb_subscribe(ORB_ID(vehicle_status));
+int _vehicle_land_detected_sub = orb_subscribe(ORB_ID(vehicle_land_detected));
+
+//Advertise to actuator_topic (voir dans exemples/uuv_exemple)
+
+// Faire les update de paramètres
+
+vehicle_setpoint_poll();
+vehicle_accel_poll();
+vehicle_control_mode_poll();
+vehicle_manual_poll();
+distance_sensor_poll();
+vehicle_status_poll();
+vehicle_land_detected_poll();
+
+// Mettre les PID
+
+//Envoyer les commandes au moteur
