5a564cad5d3fa2ea5662b2b163163b52bd940ed4
Guillaume Rozand <guillaume.rozand@epfl.ch>
2019-03-12 14:44:04 +0100

    Enlever l'essai de build moins de trucs parce que ca marche pas

--- boards/px4/fmu-v3/default.cmake
+++ boards/px4/fmu-v3/default.cmake
@@ -69,23 +69,23 @@ px4_add_board(
 		dataman
 		ekf2
 		events
-		# fw_att_control
-		# fw_pos_control_l1
-		# gnd_att_control
-		# gnd_pos_control
+		fw_att_control
+		fw_pos_control_l1
+		gnd_att_control
+		gnd_pos_control
 		land_detector
 		landing_target_estimator
 		load_mon
 		local_position_estimator
 		logger
 		mavlink
-		# mc_att_control
-		# mc_pos_control
+		mc_att_control
+		mc_pos_control
 		navigator
 		PRAO
 		sensors
 		vmount
-		# vtol_att_control
+		vtol_att_control
 		wind_estimator
 
 	SYSTEMCMDS
